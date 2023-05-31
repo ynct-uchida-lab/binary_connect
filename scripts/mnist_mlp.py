ac58c225376dbf485b9af3d1d3f3201e7fa10f71
Kyosuke1004 <kyosukeippoku297@icloud.com>
2023-05-31 15:17:23 +0900

    学習ループでもlossとaccを出せるようにし，それをグラフに追加

--- scripts/mnist_mlp.py
+++ scripts/mnist_mlp.py
@@ -65,12 +65,16 @@ def main():
     history = {
         'train_loss': [],
         'test_loss': [],
+        'train_acc': [],
         'test_acc': [],
     }
     # 学習，テスト用ループ
     for epoch in range(epochs):
         # 学習開始
         model.train()
+        train_loss = 0
+        train_correct = 0
+        train_data_num = 0
         for inputs, labels in loaders['train']:
             # 指定したdeviceに変数を変換
             inputs = inputs.to(device)
@@ -81,17 +85,23 @@ def main():
             outputs = model(inputs)
             # 誤差を算出
             loss = criterion(outputs, labels)
+            train_loss += loss.item()
             # 誤差逆伝搬とパラメーターの更新
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            # 正解数を求める
+            outputs = torch.argmax(outputs, dim=1)
+            train_correct += torch.sum(outputs == labels).item()
+            # 総データ数を求める
+            train_data_num += labels.shape[0]
     
         # テストパート
         # 学習のストップ
         model.eval()
         test_loss = 0
-        correct = 0
-        data_num = 0
+        test_correct = 0
+        test_data_num = 0
         with torch.no_grad():
             for inputs, labels in loaders['test']:
                 # 指定したdeviceに変数を変換
@@ -105,20 +115,24 @@ def main():
                 test_loss += criterion(outputs, labels).item()
                 # 正解数を求める
                 outputs = torch.argmax(outputs, dim=1)
-                correct += torch.sum(outputs == labels).item()
+                test_correct += torch.sum(outputs == labels).item()
                 # 総データ数を求める
-                data_num += labels.shape[0]
+                test_data_num += labels.shape[0]
                 
         # 平均誤差を算出
-        test_loss /= data_num
-        correct /= data_num
+        train_loss /= train_data_num
+        test_loss /= test_data_num
+        train_correct /= train_data_num
+        test_correct /= test_data_num
         # 1エポックあたりの誤差と正解率を表示
-        print('epoch{} Test loss (avg): {}, Accuracy: {}'.format(epoch + 1, test_loss, correct))
+        print('epoch{} Train loss (avg): {}, Accuracy: {}'.format(epoch + 1, train_loss, train_correct))
+        print('epoch{} Test loss (avg): {}, Accuracy: {}'.format(epoch + 1, test_loss, test_correct))
 
         # 誤差と正解率を代入
-        history['train_loss'].append(loss.item())
+        history['train_loss'].append(train_loss)
+        history['train_acc'].append(train_correct)
         history['test_loss'].append(test_loss)
-        history['test_acc'].append(correct)
+        history['test_acc'].append(test_correct)
 
     # 結果の出力と描画
     plt.figure()
@@ -127,12 +141,13 @@ def main():
     plt.xlabel('epoch')
     plt.legend()
     plt.savefig('../outputs/loss.png')
- 
+    
     plt.figure()
-    plt.plot(range(1, epochs + 1), history['test_acc'])
-    plt.title('test accuracy')
+    plt.plot(range(1, epochs + 1), history['train_acc'], label='train_acc')
+    plt.plot(range(1, epochs + 1), history['test_acc'], label='test_acc')
     plt.xlabel('epoch')
-    plt.savefig('../outputs/test_acc.png')
+    plt.legend()
+    plt.savefig('../outputs/acc.png')
     
 if __name__ == '__main__':
     main()
