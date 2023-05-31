28ad9e7f268593704a2c75195fa43e900d5d04c2
Kyosuke1004 <kyosukeippoku297@icloud.com>
2023-05-31 14:36:04 +0900

    コードの整理

--- scripts/mnist_mlp.py
+++ scripts/mnist_mlp.py
@@ -1,36 +1,35 @@
 # import modules
 import torch
-import torch.nn.functional as f
+import torch.nn as nn
 import matplotlib.pyplot as plt
-import numpy as np
 
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+from torchvision import datasets
+from torchvision import transforms
 
 import networks
 
 # NNISTをロードする関数
 def load_MNIST(batch=128, intensity=1.0):
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data',
-                       train=True,
-                       download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Lambda(lambda x: x * intensity)
-                       ])),
+    train_loader = DataLoader(
+        datasets.MNIST('../data/',
+            train=True,
+            download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Lambda(lambda x: x * intensity)
+            ])),
         batch_size=batch,
         shuffle=True)
 
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data',
-                       train=False,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Lambda(lambda x: x * intensity)
-                       ])),
-        batch_size=batch,
-        shuffle=True)
+    test_loader = DataLoader(
+        datasets.MNIST('../data/',
+            train=False,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Lambda(lambda x: x * intensity)
+            ])),
+        batch_size=batch)
     
     return {'train': train_loader, 'test': test_loader}
 
@@ -38,86 +37,88 @@ def load_MNIST(batch=128, intensity=1.0):
 # main
 # **********************************************
 def main():
+    
+    # -------------------------------------
+    # モデルの宣言
+    # -------------------------------------
+    # モデルのインスタンス
+    model = networks.MLPCls()
+    # デバイスの設定
+    device = torch.device(
+        'cuda:0' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    # 誤差関数
+    criterion = nn.CrossEntropyLoss()
+    #最適化器
+    optimizer = torch.optim.Adam(params=model.parameters())
+    # MNISTのデータローダーを取得
+    loaders = load_MNIST()
+    
+    # -------------------------------------
+    # モデルの学習
+    # -------------------------------------
     # 学習回数
     epochs = 5
-    
     # 学習結果の保存用
     history = {
         'train_loss': [],
         'test_loss': [],
         'test_acc': [],
     }
-    
-    # ネットワークを構築
-    net: torch.nn.Module = networks.MyNet()
-    
-    # MNISTのデータローダーを取得
-    loaders = load_MNIST()
-    
-    # -------------------------------------
-    # 最適化
-    # -------------------------------------
-    optimizer = torch.optim.Adam(params=net.parameters(), lr=0.001)
-    
-    # -------------------------------------
-    # モデルの学習
-    # -------------------------------------
-    
     # 学習，テスト用ループ
     for epoch in range(epochs):
-        
-        # 学習パート
-        loss = None
         # 学習開始
-        net.train(True)
-        for i, (data, target) in enumerate(loaders['train']):
-            # 学習部分
-            # 全結合のみのネットワークでは入力を1次元に
-            # print(data.shape)
-            # torch.Size([128, 1, 28, 28])
-            data = data.view(-1, 28 * 28)
-            # print(data.shape)
-            # torch.Size([128, 784])
-            
+        model.train()
+        for inputs, labels in loaders['train']:
+            # 指定したdeviceに変数を変換
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            # データの整形
+            inputs = inputs.view(-1, 28 * 28)
+            # ニューラルネットワークへのデータ入力
+            outputs = model(inputs)
+            # 誤差を算出
+            loss = criterion(outputs, labels)
+            # 誤差逆伝搬とパラメーターの更新
             optimizer.zero_grad()
-            output = net(data)
-            loss = f.nll_loss(output, target)
             loss.backward()
             optimizer.step()
-            
-            if i % 10 == 0:
-                print('Training log: {} epoch ({} / 60000 train. data). Loss: {}'.format(epoch + 1,
-                                                                                         (i + 1) * 128,
-                                                                                         loss.item())
-                      )
-        
-                
-        history['train_loss'].append(loss.detach().numpy())
     
         # テストパート
         # 学習のストップ
-        net.eval()
+        model.eval()
         test_loss = 0
         correct = 0
+        data_num = 0
         with torch.no_grad():
-            for data, target in loaders['test']:
-                # テスト部分
-                data = data.view(-1, 28 * 28)
-                output = net(data)
-                test_loss += f.nll_loss(output, target, reduction='sum').item()
-                pred = output.argmax(dim=1, keepdim=True)
-                correct += pred.eq(target.view_as(pred)).sum().item()
- 
-        test_loss /= 10000
+            for inputs, labels in loaders['test']:
+                # 指定したdeviceに変数を変換
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                # データの整形
+                inputs = inputs.view(-1, 28 * 28)
+                # ニューラルネットワークへのデータ入力
+                outputs = model(inputs)
+                # 誤差を算出
+                test_loss += criterion(outputs, labels).item()
+                # 正解数を求める
+                outputs = torch.argmax(outputs, dim=1)
+                correct += torch.sum(outputs == labels).item()
+                # 総データ数を求める
+                data_num += labels.shape[0]
+                
+        # 平均誤差を算出
+        test_loss /= data_num
+        correct /= data_num
+        # 1エポックあたりの誤差と正解率を表示
+        print('epoch{} Test loss (avg): {}, Accuracy: {}'.format(epoch+1, test_loss, correct))
 
-        print('Test loss (avg): {}, Accuracy: {}'.format(test_loss,
-                                                         correct / 10000))
-
+        # 誤差と正解率を代入
+        history['train_loss'].append(loss.item())
         history['test_loss'].append(test_loss)
-        history['test_acc'].append(correct / 10000)
+        history['test_acc'].append(correct)
 
     # 結果の出力と描画
-    print(history)
     plt.figure()
     plt.plot(range(1, epochs + 1), history['train_loss'], label='train_loss')
     plt.plot(range(1, epochs + 1), history['test_loss'], label='test_loss')
--- scripts/networks.py
+++ scripts/networks.py
@@ -1,11 +1,10 @@
 # import modules
 import torch
-import torch.nn.functional as f
 
-class MyNet(torch.nn.Module):
+class MLPCls(torch.nn.Module):
     def __init__(self):
         # ネットワークの定義
-        super(MyNet, self).__init__()
+        super(MLPCls, self).__init__()
         self.fc1 = torch.nn.Linear(28 * 28, 1000)
         self.fc2 = torch.nn.Linear(1000, 10)
         
@@ -15,5 +14,5 @@ class MyNet(torch.nn.Module):
         x = torch.sigmoid(x)
         x = self.fc2(x)
         
-        return f.log_softmax(x, dim=1)
+        return x
     
