de165616a709d5fe499bf16cee611973d8d04ad5
Kyosuke1004 <kyosukeippoku297@icloud.com>
2023-06-02 10:02:24 +0900

    ネットワークを整理しBatchNormを追加

--- scripts/networks.py
+++ scripts/networks.py
@@ -21,6 +21,7 @@ class BCCls(torch.nn.Module):
         # ネットワークの定義
         super(BCCls, self).__init__()
         self.fc1 = torch.nn.Linear(28 * 28, 1000)
+        self.bc = torch.nn.BatchNorm1d(1000)
         self.fc2 = torch.nn.Linear(1000, 10)
         self.fc1_bc = torch.nn.Linear(28 * 28, 1000)
         self.fc2_bc = torch.nn.Linear(1000, 10)
@@ -29,8 +30,9 @@ class BCCls(torch.nn.Module):
     def forward(self, x):
         self.fc1_bc.weight.data = torch.sign(self.fc1.weight.data)
         self.fc2_bc.weight.data = torch.sign(self.fc2.weight.data)
-        x = self.fc1(x)
+        x = self.fc1_bc(x)
+        x = self.bc(x)
         x = torch.relu(x)
-        x = self.fc2(x)
+        x = self.fc2_bc(x)
         
         return x
