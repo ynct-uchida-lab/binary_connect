204898ca730d9f207dd134de6f7ba4270b08a294
Kyosuke1004 <kyosukeippoku297@icloud.com>
2023-06-16 12:01:11 +0900

    mlpでも学習ループとテストループをfor文で結合

--- scripts/networks.py
+++ scripts/networks.py
@@ -7,12 +7,14 @@ class MLPCls(torch.nn.Module):
         # ネットワークの定義
         super(MLPCls, self).__init__()
         self.fc1 = torch.nn.Linear(28 * 28, 1000)
+        self.bc = torch.nn.BatchNorm1d(1000)
         self.fc2 = torch.nn.Linear(1000, 10)
         
     # 順伝搬
     def forward(self, x):
         x = self.fc1(x)
-        x = torch.sigmoid(x)
+        x = self.bc(x)
+        x = torch.relu(x)
         x = self.fc2(x)
         
         return x
